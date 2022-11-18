64afd70930a9b063b91082be6c1dd3f3d63137fd
mims <113519976+MimPMC@users.noreply.github.com>
2022-11-18 16:49:37 +0100

    Created a start screen and audio button

--- main.js
+++ main.js
@@ -1,16 +1,60 @@
 window.addEventListener("DOMContentLoaded", main);
 
+const gameContainer = document.getElementById("game-container")
+let scene = 0
+let playerName
+
 function main() {
     loadStartScene();
-   
 
 }
 
+function playSound() {
+    if (scene == 0 ) {
+        const audio = new Audio('./creepy.wav');
+        audio.volume = 0.2;
+        audio.setAttribute = "loop"
+        audio.play();
+    }
+}
+
+
 function loadStartScene() {
 
-    const p = document.createElement("p");
-    p.innerHTML = "Hej! Välkommen till Skövde.";
-    document.getElementById("game-container").appendChild('p');
-    p.className ="text"
+    
+    const startImage = document.createElement("img")
+    startImage.src = "./skovde-bg.png"
+    startImage.className = "start-bg-img"
+    gameContainer.appendChild(startImage);
 
+    const gameTitle = document.createElement("h1");
+    gameTitle.textContent = "Välkommen till Skövde";
+    gameTitle.className ="game-title"
+    gameContainer.appendChild(gameTitle);
+
+    const gameDescription= document.createElement("h3");
+    gameDescription.textContent = "Vänligen fyll i ditt namn och börja utforska staden!";
+    gameDescription.className ="game-description"
+    gameContainer.appendChild(gameDescription);
+
+    const inputName = document.createElement("input")
+    inputName.className = "input-name"
+    gameContainer.appendChild(inputName);
+
+    const buttonStart = document.createElement("button")
+    buttonStart.className = "button-start"
+    buttonStart.textContent = "Start"
+    gameContainer.appendChild(buttonStart);
+    buttonStart.addEventListener("click", addName);
+
+    function addName() {
+        playerName = inputName.value;
+        if (playerName == "") {
+            alert("Enter First Name");
+            
+        }
+        else {
+            console.log(playerName)
+        }
+    }
  }
